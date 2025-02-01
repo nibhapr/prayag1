0451d4018be60abb4cd97040fb37490f954b1345
IamShaDoW666 <www.gaming2018@gmail.com>
2025-02-01 21:44:44 +0530

    about

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,15 +1,14 @@
 // An array of links for navigation bar
 const navBarLinks = [
   { name: "Home", url: "/" },
-  // { name: "Gallery", url: "/gallery" },
+  { name: "Gallery", url: "/gallery" },
+  { name: "About", url: "/about" },
   // { name: "Services", url: "/services" },
   // { name: "Blog", url: "/blog" },
   { name: "Contact", url: "/contact" },
- 
 ];
 // An array of links for footer
 const footerLinks = [
- 
   {
     section: "Menus",
     links: [
@@ -22,11 +21,12 @@ const footerLinks = [
 ];
 // An object of links for social icons
 const socialLinks = {
-  facebook: "https://www.facebook.com/people/The-Startup-Garage/61560483126638/?mibextid=ZbWKwL",
+  facebook:
+    "https://www.facebook.com/people/The-Startup-Garage/61560483126638/?mibextid=ZbWKwL",
   x: "https://twitter.com/",
-  instagram: "https://www.instagram.com/the_startup_garage_?igsh=bnBrOXFveWdqa2t6",
+  instagram:
+    "https://www.instagram.com/the_startup_garage_?igsh=bnBrOXFveWdqa2t6",
   google: "https://www.google.com/",
-
 };
 
 export default {
